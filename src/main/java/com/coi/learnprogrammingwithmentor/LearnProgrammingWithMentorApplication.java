fe71aaa0f43eacd4f6de2e093cb210368849ee23
Евгений Фабер <coikarp175@gmail.com>
2022-07-06 16:27:34 +0600

    Made base options and added React.js

---
+++ src/main/java/com/coi/learnprogrammingwithmentor/LearnProgrammingWithMentorApplication.java
@@ -0,0 +1,13 @@
+package com.coi.learnprogrammingwithmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnProgrammingWithMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnProgrammingWithMentorApplication.class, args);
+	}
+
+}
